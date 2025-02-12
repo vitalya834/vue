996184e2178034efd9847a0505b257bc4f3bca3a
Vitaly Belykh <vitalyablyh@gmail.com>
2025-02-12 10:55:53 +0100

    02

---
+++ 02/script.js
@@ -0,0 +1,8 @@
+Vue.createApp({
+  data() {
+    return {
+      vueVersion: 3,
+      text: "<p>Directives are special attributes with the <code>v-</code> prefix. They allow us to apply dynamic behavior to our Vue templates.</p>"
+    };
+  }
+}).mount("#app");
