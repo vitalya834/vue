e1ee19230fc0d6f80edf6ad9511febec9bd54155
Vitaly Belykh <vitalyablyh@gmail.com>
2025-02-21 11:06:28 +0100

    test

---
+++ vujpro/vue-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h3>{{ info }}</h3>
+  <p>{{ someInfo }}</p>
+
+  <button type="button" @click="userDate()">Отправить</button>
+</template>
+
+<style>
+h3 {
+  font-weight: lighter;
+}
+
+p {
+  color: rgb(204, 43, 43);
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      info: "Titel!",
+      someInfo: "Anons of message",
+    };
+  },
+  methods: {
+    userDate() {
+      this.someInfo = "Some new"; // исправлена ошибка в названии переменной
+    },
+  },
+};
+</script>
